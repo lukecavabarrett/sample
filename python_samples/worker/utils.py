b3599f7d9680a1cc3a54d39eca98c88aa8622ad8
lukecavabarrett <luca.cavallery@gmail.com>
2020-02-29 21:58:17 +0000

    bug fix

--- python_samples/worker/utils.py
+++ python_samples/worker/utils.py
@@ -4,4 +4,4 @@ from . import comm_pipe
 def send_object(obj):
     comm_pipe.write(json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True))
     comm_pipe.write('\0')
-    comm_pipe.comm_pipe.flush()
+    comm_pipe.flush()
