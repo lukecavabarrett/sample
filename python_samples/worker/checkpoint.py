bba768f0cb607b5884dd75c639b6eb6fc9a0fadb
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-06 21:55:54 +0000

    improved checkpoint name collision

--- python_samples/worker/checkpoint.py
+++ python_samples/worker/checkpoint.py
@@ -9,15 +9,16 @@ def is_available():
 
 class checkpoint:
     def __init__(self):
-        self.linked_files = []
+        self.__linked_files = []
 
     def save_to_server(self):
-        self.request_type = 'save_checkpoint'
-        utils.send_object(self)
+        self.__request_type = 'save_checkpoint'
+        comm_pipe.write(json.dumps(self, default=lambda o: o.__dict__, sort_keys=True))
+        comm_pipe.write('\0')
+        comm_pipe.flush()
 
     def link_file(self, path):
-        self.linked_files.append(path)
-
+        self.__linked_files.append(path)
 
 def restore_checkpoint():
     if not os.path.exists('/tmp/__hydra_checkpoint.json'):
@@ -27,4 +28,5 @@ def restore_checkpoint():
         for key, value in json.load(json_file).items():
             setattr(ck, key, value)
         return ck
+
 # Checkpoint available: {'best_epoch': 340, 'best_epoch_loss': 0.019209275022149086, 'last_epoch': 749, 'linked_files': ['340.pkl', '749.pkl'], 'request_type': 'save_checkpoint'}
--- python_samples/worker/utils.py
+++
@@ -1,7 +0,0 @@
-import json
-from . import comm_pipe
-
-def send_object(obj):
-    comm_pipe.write(json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True))
-    comm_pipe.write('\0')
-    comm_pipe.flush()
