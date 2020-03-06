cd9bcb2de10bd191375042a66cfa8f3d137f05d8
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-06 21:16:22 +0000

    prime counting

---
+++ python_samples/save_chekpoint_no_file.py
@@ -0,0 +1,21 @@
+import time, sys
+
+from .worker import checkpoint
+
+
+if not checkpoint.is_available():
+    sys.stderr.write('borg checkpoints option not available\n')
+else:
+    sys.stderr.write('borg checkpoints option is available\n')
+
+sys.stderr.flush()
+
+time.sleep(1)
+
+ck = checkpoint.checkpoint()
+ck.jack = 'sparrow'
+ck.save_to_server()
+
+time.sleep(1)
+
+
